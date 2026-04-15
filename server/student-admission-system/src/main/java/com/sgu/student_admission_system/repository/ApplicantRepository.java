9649faace9be0064ff4813df3b0563412acbf895
Tran Dinh Khanh Du <trandinhkhanhdu2005@gmail.com>
2026-04-15 07:13:34 +0700

    update batch

--- server/student-admission-system/src/main/java/com/sgu/student_admission_system/repository/ApplicantRepository.java
+++ server/student-admission-system/src/main/java/com/sgu/student_admission_system/repository/ApplicantRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -18,4 +20,6 @@ public interface ApplicantRepository extends JpaRepository<Applicant, Integer> {
     boolean existsByPhoneNumberIn(Collection<String> phoneNumbers);
 
     Optional<Applicant> findByCccd(String cccd);
+
+    List<Applicant> findAllByCccd(Iterable<String> cccds);
 }
--- server/student-admission-system/src/main/java/com/sgu/student_admission_system/repository/MajorRepository.java
+++ server/student-admission-system/src/main/java/com/sgu/student_admission_system/repository/MajorRepository.java
@@ -3,10 +3,13 @@ package com.sgu.student_admission_system.repository;
 import com.sgu.student_admission_system.entity.Major;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MajorRepository extends JpaRepository<Major, Integer> {
     boolean existsByMajorCode(String majorCode);
+
+    List<Major> findAllByMajorCode(Iterable<String> majorCode);
 
     Optional<Major> findByMajorCode(String majorCode);
 }
